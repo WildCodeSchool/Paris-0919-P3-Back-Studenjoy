a136609f5a477c766fbdf5ccd2d71d51357708cb
Ibrahima Niass <cheroubawan@hotmail.fr>
2020-01-14 23:08:19 +0100

    finished application route for getting student choices

--- routes/student_choices/index.js
+++ routes/student_choices/index.js
@@ -18,4 +18,31 @@ router.post('/students/application', (req, res) => {
   );
 });
 
+// Get all student choices
+
+router.get('/students/application/:id', (req, res) => {
+  const studentId = req.params.id;
+  const query = `SELECT 
+  first_name, last_name, speciality_name, school_name
+  FROM
+  student
+      INNER JOIN
+  application ON application.student_id = student.id
+      INNER JOIN
+  speciality ON speciality.id = application.speciality_id
+      INNER JOIN
+  school ON school.id = application.school_id
+WHERE
+  student.id = ${studentId}`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      res
+        .send("Erreur lors de la récupération des infos de l'étudiant")
+        .status(500);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 module.exports = router;
