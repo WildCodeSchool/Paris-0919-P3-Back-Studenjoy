2826ca97fd232c19aa00eeca88ff83fb06c2242b
Ibrahima Niass <cheroubawan@hotmail.fr>
2020-01-17 00:07:50 +0100

    search by speciality and/or city and/or school

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const studentRouter = require('./routes/student');
 const choicesRouter = require('./routes/student_choices');
+const searchRouter = require('./routes/school-search');
 const connection = require('./helpers/db'); //CONNECTION TO DATABASE
 const port = process.env.PORT;
 // const util = require('util');
@@ -17,6 +18,7 @@ app.use(
 );
 app.use(studentRouter);
 app.use(choicesRouter);
+app.use(searchRouter);
 
 app.listen(port, err => {
   if (err) {
--- routes/school-search/index.js
+++ routes/school-search/index.js
@@ -5,31 +5,40 @@ const router = express.Router();
 // GET schools selon speciality et/ou ville et/ou school name
 
 router.get('/results', (req, res) => {
-  if (request.query.speciality && request.query.school && request.query.city) {
-    const query = `SELECT 
-    *
+  const school = `(school_name = "${req.query.school}")`;
+  const city = `(city = "${req.query.city}")`;
+  const speciality = `speciality_name = "${req.query.speciality}"`;
+  const and = 'AND';
+  const query = `SELECT
+  *
 FROM
-    school as sco
-        JOIN
-    school_speciality scos ON sco.id = scos.school_id
-        JOIN
-    speciality as spe ON spe.id = scos.speciality_id
- WHERE
-    speciality_name = "economy"
-    AND (school_name = "Universités de l'académie d'Aix-Marseille")
-    AND (city = 'Marseille')`;
-
-    connection.query(query, (err, results) => {
-      if (err) {
-        res
-          .send('Erreur lors de la récupération de la liste des étudiants')
-          .status(500);
-      } else {
-        console.log(results);
-        res.json(results);
-      }
-    });
+  school as sco
+      JOIN
+  school_speciality scos ON sco.id = scos.school_id
+      JOIN
+  speciality as spe ON spe.id = scos.speciality_id
+WHERE
+  ${req.query.speciality ? speciality : ''}
+  ${
+    (req.query.speciality && req.query.school) ||
+    (req.query.speciality && req.query.city)
+      ? and
+      : ''
   }
+  ${req.query.school ? school : ''}
+  ${req.query.school && (req.query.city || req.query.speciality) ? and : ''}
+
+  ${req.query.city ? city : ''}`;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      res
+        .status(500)
+        .send('Erreur lors de la récupération de la liste des étudiants');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 module.exports = router;
