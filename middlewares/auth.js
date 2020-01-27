3000ac981bafb6fffd523bdbdff4a8715dfa3dd9
Ibrahima Niass <cheroubawan@hotmail.fr>
2020-01-27 16:25:06 +0100

    updated route with auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,9 +3,11 @@ const jwt = require('jsonwebtoken');
 const auth = (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
+    console.log('token', token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded) {
-      req.id = decoded.student.id;
+      console.log('req id:', decoded.id);
+      req.id = decoded.id;
     }
     next();
   } catch (e) {
